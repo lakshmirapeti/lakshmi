9e64f87679205b1924847e41c737d33b651b1626
Venkaty9 <125430501+Venkaty9@users.noreply.github.com>
2023-09-20 13:21:28 +0530

    SUBMIT

---
+++ 1.component-code/ClassCom.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class ClassCom extends Component {
+  render() {
+    return (
+      <div>ClassCom</div>
+    )
+  }
+}
+
+export default ClassCom
---
+++ 1.component-code/FunctionalCom.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FunctionalCom() {
+  return (
+    <div>FunctionalCom</div>
+  )
+}
+
+export default FunctionalCom
